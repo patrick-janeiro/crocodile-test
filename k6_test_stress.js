dbdec8e3574551d8bc10fcfd69fe40a08ea154f7
Patrick Janeiro <p.janeiro8@gmail.com>
2023-02-09 13:40:18 -0500

    Create k6_test_stress.js

---
+++ k6_test_stress.js
@@ -0,0 +1,12 @@
+// stress-test.js
+import { sleep, check } from 'k6';
+import http from 'k6/http';
+
+
+export default () => {
+  const res = http.get('https://test-api.k6.io');
+  check(res, {
+    'status is 200': () => res.status === 200,
+  });
+  sleep(1);
+};
